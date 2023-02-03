a45b1a0e60192751fe89b5f375f31cd582d5083d
Rajneesh Dubey <dubeyrajneesh786@gmail.com>
2023-02-03 10:34:22 +0530

    first commit

---
+++ L-03 ( Conditional_And_Loops )/Fahrenheit_To_Celcius_Table.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+int S ,E,W ;
+cin>>S>>E>>W ;
+
+for(int i= S ; i<= E ; i+= W)
+{
+
+	cout<<i<< " "<<((i-32)*5)/9 <<endl;
+}
+
+
+  
+}
+
+
