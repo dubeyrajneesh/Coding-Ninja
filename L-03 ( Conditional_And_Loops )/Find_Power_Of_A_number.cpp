3d3486d3b7ab1416e74afae6677bc22ca70361ae
Rajneesh Dubey <dubeyrajneesh786@gmail.com>
2023-02-03 10:51:29 +0530

    first commit

---
+++ L-03 ( Conditional_And_Loops )/Find_Power_Of_A_number.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int power(int x , int n)
+{
+
+	if(n==0)
+	{
+		return 1 ;
+	}
+
+	else
+	{
+
+		return x * power(x , n-1) ;
+	}
+}
+
+int main() {
+	// Write your code here
+
+	int x , n;
+	cin>>x>>n;
+
+	cout<<power(x ,n)<<endl;
+	
+}
